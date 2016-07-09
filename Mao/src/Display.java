fd8c63ffee4390545b9eb69a2591b2e7e6aac48d
seanlindstrom <Sean@10.0.1.18>
2016-07-08 23:25:11 -0400

    Beginning of Display class

---
+++ Mao/src/Display.java
@@ -0,0 +1,41 @@
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+
+public class Display {
+	
+	private long window;
+	
+	public Display() {
+		init();
+	}
+	
+	public void init() {
+		
+		if (glfwInit() != GL_TRUE) {
+		    System.err.println("Error initializing GLFW");
+		    System.exit(1);
+		}
+		
+		window = glfwCreateWindow(1280, 720, "Mao", 0, NULL);
+		
+		if (windowID == NULL)
+		{
+		    System.err.println("Error creating a window");
+		    System.exit(1);
+		}
+		
+		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+		glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
+		
+	}
+	
+	public void terminate() {
+		
+		glfwDestroyWindow(window);
+		
+		glfwTerminate();
+		
+	}
+	
+	
+}
